038c8e3a7455ce686a60b4bacc1dc5b45eae3369
honnguyen <honnguyen@ssis.edu.vn>
2019-10-04 13:59:12 +0700

    rm adminview

--- signin/urls.py
+++
@@ -1,8 +0,0 @@
-# from django.urls import path
-from django.conf.urls import include, url
-from . import views
-
-
-urlpatterns = [
-    url('', views.welcome, name = 'index.html')
-]
--- urls.py
+++ urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-# from django.urls import path, include
 from django.conf.urls import include, url
-from . import views
+from tutorview import views
 admin.autodiscover()
 urlpatterns = [
     # url('', include(admin.site.urls)),
-    url('', views.welcome, name = 'index.html')
-    # url(r'^login/?$', login, {'template_name': 'index.html'}, name='login')
+    url('', views.tutorview, name = 'index.html')
 ]
