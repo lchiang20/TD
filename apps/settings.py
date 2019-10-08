a0c082ff0c6a0d91f08efd027dd0dd53f19fa829
lchiang20 <lchiang20@ssis.edu.vn>
2019-10-08 14:44:55 +0700

    static

--- apps/settings.py
+++ apps/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'tutorview/static')
+    os.path.join(BASE_DIR, 'static')
 ]
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'tutorview/static/assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/assets')
